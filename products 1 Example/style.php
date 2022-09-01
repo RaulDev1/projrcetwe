73d32f1a7b20d0ce660260a6bff77b832b1b119e
RaulDev1 <110821205+RaulDev1@users.noreply.github.com>
2022-09-01 14:28:02 +0400

    Add files via upload

---
+++ products 1 Example/style.php
@@ -0,0 +1,91 @@
+.mbox{ max-width:1160px; }
+#pagebox.box{max-width:1200px; }
+
+#mchatbutton{ background-color:#4499f0; }
+.qqlv {border: 5px solid #4499f0;}
+a:link,a:visited,a:active,a:hover {color:#4499f0;}
+a.rmore{ background-color:#4499f0;}
+#searchform #searchsubmit:hover{ background-color:#4499f0;border:1px solid #4499f0;}
+#access {
+	background-color:#4499f0;
+}
+#primary li ul li a:hover,#primary li ul li.current-cat a,#linksa a:hover,#primary li .children li.current-cat a  {color:#4499f0;}
+#primary li ul li.current-cat a,#primary li.topli ul li.current_page_item a,#primary li ul li a:hover,#primary li.topli ul li:hover  a{color: #4499f0;}
+
+#mpic .menquiry,#mpic2 .menquiry,#sdiv1 .menquiry{ background-color:#4499f0; }
+
+#primary li.topli h3{background-color:#4499f0; }
+
+h3.mtitle span{ background-color:#4499f0;}
+#searchform #searchsubmit{border:1px solid #4499f0; background-color:#4499f0;}
+#access li:hover > a{
+	 background-color:#4499f0;
+}
+#access ul li.current_page_item > a,
+#access ul li.current-menu-ancestor > a,
+#access ul li.current-menu-item > a,
+#access ul li.current-menu-parent > a {
+	background-color:#4499f0;
+
+}
+* html #access ul li.current_page_item a,
+* html #access ul li.current-menu-ancestor a,
+* html #access ul li.current-menu-item a,
+* html #access ul li.current-menu-parent a,
+* html #access ul li a:hover {
+	background-color:#4499f0;
+}
+#access2 li:hover > a{
+	color:#4499f0;
+
+}
+#access2 ul li.current_page_item > a,
+#access2 ul li.current-menu-ancestor > a,
+#access2 ul li.current-menu-item > a,
+#access2 ul li.current-menu-parent > a {
+	color:#4499f0;
+
+
+}
+* html #access2 ul li.current_page_item a,
+* html #access2 ul li.current-menu-ancestor a,
+* html #access2 ul li.current-menu-item a,
+* html #access2 ul li.current-menu-parent a,
+* html #access2 ul li a:hover {
+	color:#4499f0;
+}
+#primary h1 span,
+#primary h2 span,
+#primary h3 span,
+#primary h4 span,
+#primary h5 span,
+#primary h6 span{   background-color:#4499f0; }
+input[type="submit"]{border:0px solid #4499f0; width:150px; margin:0; background-color:#4499f0; }
+#access2 li:hover > a{
+	color:#4499f0;
+
+}
+
+#access2 ul ul li:hover > a {
+	color:#4499f0;
+}
+#mhomebox{border:#4499f0 3px solid;}
+#searchform #searchsubmit{background-color:#4499f0;border:1px solid #4499f0;}
+#searchform #searchsubmit:hover{ background-color:#4499f0;border:1px solid #4499f0;}
+
+@media screen and (max-width:1160px) { 
+#wrapper{width:100%;}
+}
+@media only screen and (min-width: 480px) and (max-width: 767px) { 
+#access2 ul ul li:hover > a {
+	color:#4499f0;
+}
+#wrapper{width:100%;}
+}
+.mtab UL LI.on 
+{ 
+background: #4499f0;
+} 
+.wp-pagenavi a:hover { border: 1px solid #4499f0 !important; color: #4499f0 !important;}
+.wp-pagenavi span.current { background: #4499f0 !important; border: 1px solid #4499f0 !important;}
+.wp-pagenavi span.pages { border: 1px solid #4499f0;}
